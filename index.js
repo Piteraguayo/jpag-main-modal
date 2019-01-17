c79099f97bf373e407ad0849178cf0b800655b72
Pedro Aguayo <pedro.aguayo@exp360.com>
2019-01-17 17:17:32 +0100

    Testing JQUERY  FUNCTION

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('jquery')
 var mainModal = {
     helloWorld : function(){
         console.log("HELLO WORLD");
+    },
+
+    testJquery : function(){
+        var container = $("#mainModal");
+        container.css('display', 'none');
     }
 }
 
