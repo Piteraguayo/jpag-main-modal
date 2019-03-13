a552a64f43dbe56b944f4a4428eb51f5291a96c6
Pedro Aguayo <pedro.aguayo@exp360.com>
2019-03-13 13:42:21 +0100

    VERSION 1.0.3 added timeout to init

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var mainModal = {
-    id : "jpagMainModal",
-    defaultAcceptLabel : "Continue",
-    defaultCancelLabel : "Cancel",
-    acceptLabel : "",
-    cancelLabel : "",
+    id : 'jpagMainModal',
+    defaultAcceptLabel : 'Continue',
+    defaultCancelLabel : 'Cancel',
+    acceptLabel : '',
+    cancelLabel : '',
     onAccept : null,
     onCancel : null,
     onLoad : null,
@@ -11,86 +11,86 @@ var mainModal = {
     showCancel : false,
     params : {},
 
-    getContainerJQ : function(){
-      return   $('#'+this.id)
+    getContainerJQ : function() {
+      return   $('#'+this.id);
     },
 
-    getFooterDOM : function(){
-        return document.getElementById("mainModalFooter");
+    getFooterDOM : function() {
+        return document.getElementById('mainModalFooter');
     },
 
-    getAcceptButtonDOM : function(){
-        return document.getElementById("mainModalAcceptButton");
+    getAcceptButtonDOM : function() {
+        return document.getElementById('mainModalAcceptButton');
     },
 
-    getCancelButtonDOM : function(){
-        return document.getElementById("mainModalCancelButton");
+    getCancelButtonDOM : function() {
+        return document.getElementById('mainModalCancelButton');
     },
 
-    getMainContentDOM : function(){
-        return document.getElementById("mainContentModal");
+    getMainContentDOM : function() {
+        return document.getElementById('mainContentModal');
     },
 
-    onAcceptDo : function(){
+    onAcceptDo : function() {
         this.onAccept();
     },
 
-    onCancelDo : function(){
+    onCancelDo : function() {
         this.onCancel();
         this.hide();
     },
 
-    onLoadDo : function(){
-        if(this.onLoad!=null)  this.onLoad();
+    onLoadDo : function() {
+        if (this.onLoad != null)  this.onLoad();
     },
 
-    show : function(){
+    show : function() {
         this.createHTML();
         this.getContainerJQ().modal('show');
         this.onLoadDo();
     },
 
-    hide : function(){
+    hide : function() {
         this.getContainerJQ().modal('hide');
     },
 
-    setClickOutside : function(){
+    setClickOutside : function() {
         this.getContainerJQ().modal({backdrop: true, keyboard: true, show:false});
     },
 
-    disableClickOutside : function(){
+    disableClickOutside : function() {
         this.getContainerJQ().modal({backdrop: 'static', keyboard: false, show:false});
     },
 
-    setContent : function(modalContent){
-        this.getMainContentDOM().innerHTML=modalContent;
+    setContent : function(modalContent) {
+        this.getMainContentDOM().innerHTML = modalContent;
     },
 
-    showWithContent : function(content){
+    showWithContent : function(content) {
         this.setContent(content);
         this.show();
     },
 
-    showContentFromServer : function(urlServer, postData){
+    showContentFromServer : function(urlServer, postData) {
         $.post(urlServer, postData, function(data){
             mainModal.showWithContent(data);
         });
     },
 
-    showFooterDo : function(){
-        this.getFooterDOM().style.display="block";
+    showFooterDo : function() {
+        this.getFooterDOM().style.display = 'block';
     },
 
     hideFooter : function(){
-        this.getFooterDOM().style.display="none";
+        this.getFooterDOM().style.display = 'none';
     },
 
     showCancelDo : function(){
-        this.getCancelButtonDOM().style.display="block";
+        this.getCancelButtonDOM().style.display = 'block';
     },
 
     hideCancel : function(){
-        this.getCancelButtonDOM().style.display="none";
+        this.getCancelButtonDOM().style.display = 'none';
     },
 
     createHTML : function(){
@@ -117,39 +117,44 @@ var mainModal = {
 
             $('body').append(HTMLmodal);
         }
+    },
+
+    init : function(){
+        mainModal.createHTML();
+
+        mainModal.onAccept = mainModal.hide;
+        mainModal.onCancel = mainModal.hide;
+        mainModal.acceptLabel = mainModal.defaultAcceptLabel;
+        mainModal.cancelLabel = mainModal.defaultCancelLabel;
+
+        mainModal.getContainerJQ().on('hide.bs.modal', function (e) {
+            mainModal.showFooter = false;
+            mainModal.showCancel = false;
+            mainModal.onAccept = mainModal.hide;
+            mainModal.onCancel = mainModal.hide;
+            mainModal.onLoad = null;
+            mainModal.params = {};
+            mainModal.acceptLabel = mainModal.defaultAcceptLabel;
+            mainModal.cancelLabel = mainModal.defaultCancelLabel;
+        });
+
+        mainModal.getContainerJQ().on('show.bs.modal', function (e) {
+            mainModal.getAcceptButtonDOM().innerHTML = mainModal.acceptLabel;
+            mainModal.getCancelButtonDOM().innerHTML = mainModal.cancelLabel;
+
+            if (mainModal.showFooter) {
+                mainModal.showFooterDo();
+            }else{
+                mainModal.hideFooter();
+            }
+
+            if (mainModal.showCancel) {
+                mainModal.showCancelDo();
+            }else{
+                mainModal.hideCancel();
+            }
+        });
     }
 };
-mainModal.createHTML();
 
-mainModal.onAccept= mainModal.hide;
-mainModal.onCancel= mainModal.hide;
-mainModal.acceptLabel=mainModal.defaultAcceptLabel;
-
-mainModal.getContainerJQ().on('hide.bs.modal', function (e) {
-    mainModal.showFooter=false;
-    mainModal.showCancel=false;
-    mainModal.onAccept= mainModal.hide;
-    mainModal.onCancel= mainModal.hide;
-    mainModal.onLoad=null;
-    mainModal.params={};
-    mainModal.acceptLabel=mainModal.defaultAcceptLabel;
-    mainModal.cancelLabel=mainModal.defaultCancelLabel;
-});
-
-mainModal.getContainerJQ().on('show.bs.modal', function (e) {
-    mainModal.getAcceptButtonDOM().innerHTML=mainModal.acceptLabel;
-    mainModal.getCancelButtonDOM().innerHTML=mainModal.cancelLabel;
-
-    if(mainModal.showFooter){
-        mainModal.showFooterDo();
-    }else{
-        mainModal.hideFooter();
-    }
-
-    if(mainModal.showCancel){
-        mainModal.showCancelDo();
-    }else{
-        mainModal.hideCancel();
-    }
-});
-
+setTimeout(mainModal.init, 500);
